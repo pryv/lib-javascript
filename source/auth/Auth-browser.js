60e4250e7235ae916d4b58a852a932bc0adbb115
Perki <pm@perki.com>
2015-08-03 19:28:03 +0200

    Removed references to cloudfront

--- source/auth/Auth-browser.js
+++ source/auth/Auth-browser.js
@@ -20,7 +20,7 @@ _.extend(Auth.prototype, {
     registerURL: {ssl: true, host: 'reg.pryv.io'},
     registerStagingURL: {ssl: true, host: 'reg.pryv.in'},
     localDevel : false,
-    sdkFullPath: 'https://dlw0lofo79is5.cloudfront.net/lib-javascript/latest'
+    sdkFullPath: 'https://api.pryv.com/lib-javascript/latest'
   },
   state: null,  // actual state
   window: null,  // popup window reference (if any)
